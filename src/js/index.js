979a64dc5f8ab20fb8b77f782efa37197c6a4c78
Juan L <lauredojuan@gmail.com>
2020-07-24 21:41:56 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,31 @@
-/* eslint-disable */
-
-import "../assets/img/rigo-baby.jpg";
 import "../assets/img/4geeks.ico";
-//import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
+let color = ["red", "pink", "blue", "green", "yellow", "tomato", "purple"];
+let parent = document.querySelector(".container");
+
+function ramdomColor(){
+    return color.[Math.floor(Math.random() * color.length)];
+}
+
+let popFunction = event => {
+  console.log(event.target.id);
+};
+
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  for (let i = 0; i < 5; i++) {
+    let row = document.createElement("div");
+    row.classList.add("classRow");
+
+    parent.appendChild(row);
+    for (let j = 0; j < 5; j++) {
+      let cell = document.createElement("div");
+      cell.classList.add("classCell");
+      cell.setAttribute("id", j + i);
+      cell.style.background = ramdomColor();
+      //   cell.classList.add(ramdomColor());
+      cell.addEventListener("click", popFunction);
+      row.appendChild(cell);
+    }
+  }
 };
