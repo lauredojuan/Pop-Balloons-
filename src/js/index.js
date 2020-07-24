b999aecd88cdcb3e2a3efcae4f318ec8df46b7e8
Juan L <lauredojuan@gmail.com>
2020-07-24 23:14:26 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -4,27 +4,31 @@ import "../style/index.scss";
 let color = ["red", "pink", "blue", "green", "yellow", "tomato", "purple"];
 let parent = document.querySelector(".container");
 
-function ramdomColor(){
-    return color.[Math.floor(Math.random() * color.length)];
+function randomColor() {
+  return color[Math.floor(Math.random() * color.length)];
 }
 
-let popFunction = event => {
-  console.log(event.target.id);
-};
+function pop(event) {
+  let elementId = event.target.id;
+  //   console.log(elementId);
+  let balloon = document.getElementById(elementId);
+  //   console.log(balloon);
+  balloon.style.background = "white";
+}
 
 window.onload = function() {
   for (let i = 0; i < 5; i++) {
     let row = document.createElement("div");
     row.classList.add("classRow");
-
     parent.appendChild(row);
+
     for (let j = 0; j < 5; j++) {
       let cell = document.createElement("div");
       cell.classList.add("classCell");
-      cell.setAttribute("id", j + i);
-      cell.style.background = ramdomColor();
+      cell.setAttribute("id", j.toString() + i.toString());
+      cell.style.background = randomColor();
       //   cell.classList.add(ramdomColor());
-      cell.addEventListener("click", popFunction);
+      cell.addEventListener("click", pop);
       row.appendChild(cell);
     }
   }
