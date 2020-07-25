2d1294a32e4a15eba31eba68e76e2330d68f0cad
Juan Lauredo <juanlauredo@Juans-MBP.hsd1.fl.comcast.net>
2020-07-24 21:12:54 -0400

    reload

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ import "../style/index.scss";
 
 let color = ["red", "pink", "blue", "green", "yellow", "tomato", "purple"];
 let parent = document.querySelector(".container");
+let counter = 0;
 
 function randomColor() {
   return color[Math.floor(Math.random() * color.length)];
@@ -14,15 +15,20 @@ function pop(event) {
   let balloon = document.getElementById(elementId);
   //   console.log(balloon);
   balloon.style.background = "white";
+  counter -= 1;
+  if (counter == 0) {
+    location.reload();
+  }
 }
 
-window.onload = function() {
+window.onload = function () {
   for (let i = 0; i < 5; i++) {
     let row = document.createElement("div");
     row.classList.add("classRow");
     parent.appendChild(row);
 
     for (let j = 0; j < 5; j++) {
+      counter += 1;
       let cell = document.createElement("div");
       cell.classList.add("classCell");
       cell.setAttribute("id", j.toString() + i.toString());
